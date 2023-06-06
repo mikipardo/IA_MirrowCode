d8c3f3b12099584c802fa8cc8ae55c415c3607d9
Miki Pardo <92552184+mikipardo@users.noreply.github.com>
2023-06-06 11:32:45 +0200

    Update pinblock2.java

--- pinblock2.java
+++ pinblock2.java
@@ -1,1 +1,59 @@
 
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
+public class PinBlockGenerator {
+    public static void main(String[] args) throws Exception {
+        // Datos de entrada
+        String pin = "1234";
+        String accountNumber = "1234567890123456";
+        String key = "0123456789ABCDEF0123456789ABCDEF";
+
+        // Generar el PIN block
+        String pinBlock = generatePinBlock(pin, accountNumber, key);
+        System.out.println("PIN Block: " + pinBlock);
+    }
+
+    public static String generatePinBlock(String pin, String accountNumber, String key) throws Exception {
+        // Formatear el PIN en el formato ISO 9564-1 formato 4
+        String formattedPin = formatPin(pin);
+
+        // Concatenar el PIN formateado con los dígitos más a la derecha del número de cuenta
+        String data = formattedPin + accountNumber.substring(accountNumber.length() - 12);
+
+        // Obtener la clave de cifrado
+        byte[] keyBytes = hexStringToByteArray(key);
+        SecretKeySpec secretKeySpec = new SecretKeySpec(keyBytes, "AES");
+
+        // Crear un objeto de cifrado AES ECB sin relleno
+        Cipher cipher = Cipher.getInstance("AES/ECB/NoPadding");
+        cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec);
+
+        // Cifrar los datos
+        byte[] encryptedData = cipher.doFinal(data.getBytes(StandardCharsets.UTF_8));
+
+        // Obtener los primeros 8 bytes del resultado cifrado como el PIN block
+        byte[] pinBlockBytes = Arrays.copyOf(encryptedData, 8);
+
+        // Convertir los bytes del PIN block a una cadena hexadecimal
+        String pinBlock = bytesToHexString(pinBlockBytes);
+
+        return pinBlock;
+    }
+
+    private static String formatPin(String pin) {
+        String formattedPin = "04" + pin + "FFFFFFFF";
+        return formattedPin.substring(0, 16);
+    }
+
+    // Resto del código igual que en el ejemplo anterior
+    // ...
+  
+  /*En este método, la función formatPin() se encarga de formatear el PIN según el formato 4 de ISO 9564-1. A la cadena del PIN se le agrega "04" al inicio y se completan los dígitos faltantes con "F". Luego, se toman los primeros 16 caracteres de la cadena formateada utilizando substring(0, 16) para asegurarse de que tenga una longitud de 16 caracteres.
+
+Después de formatear el PIN, se sigue el mismo proceso de concatenar el PIN formateado con los dígitos más a la derecha del número de cuenta y realizar el cifrado utilizando el algoritmo AES en modo ECB sin relleno.
+
+Ten en cuenta que este código también asume que el número de cuenta tiene al menos 12 dígitos, al igual que en el ejemplo anterior. Asegúrate de proporcionar valores válidos para el PIN, el número de cuenta y la clave de cifrado al ejecutar el código.*/
+}

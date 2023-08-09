129a79b82e0ba6e6cca6cf00da7e7c004c59049b
Miki Pardo <92552184+mikipardo@users.noreply.github.com>
2023-08-09 12:58:23 +0200

    Update token.js

--- token.js
+++ token.js
@@ -1,4 +1,11 @@
+//Por supuesto, estaré encantado de explicarte el código línea por línea:
+
+//javascript
+
+const Imap = require('imap');
 const simpleOAuth2 = require('simple-oauth2');
+//Aquí estamos importando las bibliotecas necesarias: imap para la conexión IMAPS y simple-oauth2 para manejar la autenticación OAuth 2.0.
+
 
 const oauth2 = simpleOAuth2.create({
   client: {
@@ -11,29 +18,70 @@ const oauth2 = simpleOAuth2.create({
     tokenPath: '/common/oauth2/v2.0/token'
   }
 });
+//Creamos una instancia de simple-oauth2 con las credenciales de cliente (client ID y client secret) y las rutas de autorización y token para el flujo OAuth 2.0.
+
 
 const authorizationUri = oauth2.authorizationCode.authorizeURL({
-  redirect_uri: 'http://localhost:3000/callback', // Cambia esta URL a la que corresponda
-  scope: 'openid profile offline_access https://outlook.office365.com/mail.read', // Cambia los ámbitos según tus necesidades
+  redirect_uri: 'http://localhost:3000/callback',
+  scope: 'offline_access https://outlook.office365.com/IMAP.AccessAsUser.All',
   state: 'una_cadena_aleatoria_generada'
 });
 
 console.log('Autoriza esta aplicación visitando esta URL:', authorizationUri);
-
+Generamos una URL de autorización que el usuario debe visitar para autorizar la aplicación. Esta URL incluye la URL de redirección, los ámbitos solicitados y un estado aleatorio para seguridad.
+javascript
+Copy code
 const tokenConfig = {
-  code: 'el_codigo_de_autorizacion_recibido', // Cambia esto al código de autorización que recibas
-  redirect_uri: 'http://localhost:3000/callback' // Cambia esto a la URL correcta
+  code: 'el_codigo_de_autorizacion_recibido',
+  redirect_uri: 'http://localhost:3000/callback'
 };
+//Configuramos los detalles necesarios para intercambiar el código de autorización por un token de acceso.
 
 async function getToken() {
   try {
     const result = await oauth2.authorizationCode.getToken(tokenConfig);
     const accessToken = oauth2.accessToken.create(result);
     console.log('Token de acceso:', accessToken.token.access_token);
+    return accessToken.token.access_token;
   } catch (error) {
     console.error('Error al obtener el token:', error.message);
   }
 }
+//Definimos una función asincrónica getToken que intercambia el código de autorización por un token de acceso. Esta función devuelve el token de acceso.
 
-// Llamada a la función para obtener el token
-getToken();
+async function connectToImap() {
+  const accessToken = await getToken();
+
+  const config = {
+    user: 'tu_correo@dominio.com',
+    xoauth2: accessToken,
+    host: 'outlook.office365.com',
+    port: 993,
+    tls: true,
+    debug: {
+      enabled: true
+    }
+  };
+
+  const imap = new Imap(config);
+
+  imap.once('ready', function() {
+    console.log('Conexión IMAPS establecida');
+
+    // Aquí puedes realizar operaciones IMAP como leer correos, etc.
+
+    imap.end();
+  });
+
+  imap.once('error', function(err) {
+    console.log('Error en la conexión IMAPS:', err);
+  });
+
+  imap.connect();
+}
+//Definimos una función asincrónica connectToImap que utiliza el token de acceso obtenido para establecer una conexión IMAPS. Esta función también define eventos para manejar la conexión exitosa y los errores.
+
+// Llamada a la función para conectarse a IMAPS
+connectToImap();
+//Finalmente, llamamos a la función connectToImap para iniciar el proceso de conexión IMAPS.
+//Recuerda que este es un ejemplo simplificado y que en una aplicación real, deberías manejar de manera más segura las credenciales, las URLs de redirección y el manejo de tokens.
